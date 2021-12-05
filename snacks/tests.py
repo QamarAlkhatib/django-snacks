afd203072f13cae4f2475d5ba6dc617d8d107b5d
Qamar <qalkhatib0@gmail.com>
2021-12-05 17:22:03 +0200

    finished

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class SnacksTests(SimpleTestCase):
+    def test_homepage_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code,200)
+
+    def test_about_us_page_status_code(self):
+        url = reverse('about-us')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code,200)
+
+    def test_homepage_templates(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, "home.html")
+    def test_about_us_templates(self):
+        url = reverse('about-us')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, "about-us.html")
--- snacks/views.py
+++ snacks/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
 # Create your views here.
+# rendring HTML pages
+#class based views, another way-> function based views
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+class AboutView(TemplateView):
+    template_name = 'about-us.html'
